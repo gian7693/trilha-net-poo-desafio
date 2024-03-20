88c73448d2780836d168e7bf0970c87ba55aa59d
Gian Makoto Fujimura Lopes <gianmakoto@hotmail.com>
2024-03-20 09:34:28 -0300

    Desafio concluido

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new Iphone(numero: "15991165528", modelo: "14 Pro Max", imei: "111111111", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new Nokia(numero: "15991256847", modelo: "C01 Plus", imei: "222222222", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
